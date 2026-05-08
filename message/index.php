0a5daa0fd82e701ae96f10e4b55a0a4834cc4cf5
sugimoto-kozo <129915625+sugimoto-kozo@users.noreply.github.com>
2026-05-08 11:51:57 +0900

    gitignoreの修正

--- message/index.php
+++ message/index.php
@@ -5,8 +5,8 @@
   <meta name="viewport" content="width=device-width">
   <meta name="format-detection" content="telephone=no">
   <meta name="keywords" content="WEB,ウェブサイト,ウェブシステム,制作,名古屋">
-  <meta name="description" content="有限会社グート代表の中村彰宏からのメッセージ。ネットとの出会い、仕事への想い、これからのことについてお話しします。">
-  <title>代表メッセージ | ウェブサイト制作 グート</title>
+  <meta name="description" content="グートは、名古屋を中心に活動している、WEB制作会社です。サイトの企画・立案から、設計、制作、システム開発、運用、SEMまでを一貫してお引き受けいたします。">
+  <title>メッセージ｜ウェブサイト制作 グート</title>
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -16,7 +16,6 @@
   <link rel="stylesheet" href="/common/css/global.css">
   <link rel="stylesheet" href="/common/css/base.css?<?php echo date('Ymd-Hi'); ?>">
   <link rel="stylesheet" href="message.css?<?php echo date('Ymd-Hi'); ?>">
-
   <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"
     onerror="this.onerror=null;this.src='/common/js/jquery-3.7.1.min.js'"></script>
   <script defer src="https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.min.js"></script>
@@ -25,61 +24,51 @@
 
   <?php include_once $_SERVER['DOCUMENT_ROOT']."/common/include/tracking.php"; ?>
 </head>
-<body>
+<body id="message" class="pages">
 <?php include $_SERVER['DOCUMENT_ROOT']."/common/include/pageHead.php"; ?>
-
   <main>
 
     <div class="title-a">
       <div class="container">
         <div class="icosahedron"></div>
-        <p class="font-01">Message</p>
-        <h1>代表メッセージ</h1>
+        <h1>Message</h1>
       </div>
     </div>
 
-    <div id="breadcrumb">
-      <div class="container">
-        <ol class="breadcrumb-b font-01">
-          <li><a href="/">TOP</a></li>
-          <li>Message</li>
-        </ol>
-      </div>
+    <div class="l_col-breadcrumbs">
+      <a href="#">上位階層のページタイトル</a>
+      <ul class="l_col-link">
+        <li><a href="#">TOP</a></li>
+        <li><a href="#">Contents1</a></li>
+        <li>Contents2</li>
+      </ul>
     </div>
 
-    <section id="message">
-      <div class="container">
-
-        <div class="l_col-2">
-
-          <div class="text">
-[git test 12:19]
-            <p class="intro">有限会社グート代表の中村彰宏です。</p>
-            <p>新しいものや面白いことを見つけるのが好きで、気になることはつい調べたり試したりしてしまいます。</p>
-            <p>現在の仕事に至るきっかけも、若かりし頃、音楽を聴くのが好きだったことから始まります。Apple MusicもYouTubeもない、インターネットがまだ今ほど普及していなかった時代、さまざまなジャンルの音源をインターネット上で夢中になって探し回るうちに、自然とネットの知識が身につき、それが今の仕事へとつながっていきました。</p>
-            <p>これまでに、さまざまな業種や地域で、お客様の活動を支えるお手伝いをしてきました。大切にしているのは、使う人にとって自然で、日常の中で役立ち続ける仕組みを届けることです。</p>
-            <p>仕事では「お客様と自分たちのテリトリーを明確にする」ことを大事にしています。相手の専門分野にはあえて踏み込まず、まずは聞き役に徹します。しかし、自分たちの領域であれば間違いは正す。その姿勢が、長く続く関係の秘訣だと思っています。</p>
-            <p>趣味は映画や音楽鑑賞、ドライブ。最近はNetflixで過ごす時間も増えました。</p>
-            <p>これからは、ネット以外の分野でも、自分が持っている経験を若い世代に伝えていけたらと考えています。</p>
-            <p class="name font-01">有限会社グート　代表取締役<br><strong>中村 彰宏</strong></p>
-          </div>
-
-          <div class="media">
-            <figure class="radius">
-              <img src="/common/message/ph_message.jpg" alt="有限会社グート 代表取締役 中村彰宏">
-              <figcaption>仕事用に初めて購入したPowerBook G4。このノートPCとAdobeスイート、そしてモリサワフォントをそろえたら、貯金がほぼなくなりました（笑）</figcaption>
-            </figure>
-            <div class="btn-a">
-              <a href="/contact/">お問い合わせフォーム</a>
-            </div>
-          </div>
-
-        </div>
+    <section id="" class="container">
+      <div class="title-c">
+        <p class="en font-01">Message</p>
+        <h3 class="title"><span>メッセージ</span></h3>
       </div>
+
+      <div class="text">
+        <p>有限会社グート代表の中村彰宏です。<br>新しいものや面白いことを見つけるのが好きで、気になることはつい調べたり試したりしてしまいます。</p>
+        <p>現在の仕事に至るきっかけも、若かりし頃、音楽を聴くのが好きだったことから始まります。Apple MusicもYouTubeもない、インターネットがまだ今ほど普及していなかった時代、さまざまなジャンルの音源をインターネット上で夢中になって探し回るうちに、自然とネットの知識が身につき、それが今の仕事へとつながっていきました。</p>
+        <p>これまでに、さまざまな業種や地域で、お客様の活動を支えるお手伝いをしてきました。大切にしているのは、使う人にとって自然で、日常の中で役立ち続ける仕組みを届けることです。</p>
+        <p>仕事では「お客様と自分たちのテリトリーを明確にする」ことを大事にしています。相手の専門分野にはあえて踏み込まず、まずは聞き役に徹します。しかし、自分たちの領域であれば間違いは正す。その姿勢が、長く続く関係の秘訣だと思っています。</p>
+        <p>趣味は映画や音楽鑑賞、ドライブ。最近はNetflixで過ごす時間も増えました。</p>
+        <p>これからは、ネット以外の分野でも、自分が持っている経験を若い世代に伝えていけたらと考えています。</p>
+      </div>
+
+      <figure><img src="images/img_message.jpg" alt="メッセージ">
+    <figcaption>仕事用に初めて購入したPowerBook G4。<br>このノートPCとAdobeスイート、そしてモリサワフォントをそろえたら、貯金がほぼなくなりました（笑）</figcaption></figure>
     </section>
 
-  </main>
 
-<?php include $_SERVER['DOCUMENT_ROOT']."/common/include/pageFoot.php"; ?>
+
+
+
+
+  </main>
+  <?php include $_SERVER['DOCUMENT_ROOT']."/common/include/pageFoot.php"; ?>
 </body>
 </html>
